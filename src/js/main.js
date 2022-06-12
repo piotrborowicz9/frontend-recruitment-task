64ce9673c04386e171568de6373eeaddd329dec3
Piotr <piotrborowicz9@gmail.com>
2022-06-12 17:51:51 +0200

    finished task

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,60 @@
-"use strict";
+'use strict';
 
-const modal = document.querySelector(`.modal`);
-const paragraph = document.querySelector(`.dupa`);
-const overlay = document.querySelector(`.overlay`);
-const btnShowModal = document.querySelector(`.show-modal`);
-const btnCloseModal = document.querySelector(`.close-modal`);
-const btnResetCounter = document.querySelector(`.reset-counter`);
-let counter = 0;
+//variables
+const modal = document.querySelector('.modal');
+const paragraph = document.querySelector('.alert-content');
+const overlay = document.querySelector('.overlay');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnResetCounter = document.querySelector('.reset-counter');
+let counter = parseInt(localStorage.getItem('counter') ?? '0');
 
-const showModal = function () {
-  modal.classList.remove(`hidden`);
-  overlay.classList.remove(`hidden`);
+//functions to start/stop user modal session
+const modalSessionStart = () => {
+    counter += 1;
+    window.localStorage.setItem('counter', counter);
 };
-const closeModal = function () {
-  modal.classList.add(`hidden`);
-  overlay.classList.add(`hidden`);
+const modalSessionStop = () => {
+    counter = 0;
+    window.localStorage.setItem('counter', counter);
 };
 
-btnShowModal.addEventListener("click", function () {
-  counter += 1;
-  if (counter === 5) {
-    btnResetCounter.classList.remove(`hidden`);
-  }
-  showModal();
-  paragraph.textContent = `You clicked button ${counter} times`;
+//functions to show/close modal
+const showModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+//event listeners to show/close modal, start/reset user session
+btnShowModal.forEach((e) => {
+    e.addEventListener('click', () => {
+        modalSessionStart();
+        if (counter >= 5) {
+            btnResetCounter.classList.remove('hidden');
+        }
+
+        showModal();
+        paragraph.innerHTML = `You have clicked <b>${counter} times</b> to related button`;
+    });
 });
 
-btnResetCounter.addEventListener(`click`, function () {
-  counter = 0;
-  btnResetCounter.classList.add(`hidden`);
-  paragraph.textContent = `You clicked button ${counter} times`;
+btnResetCounter.addEventListener('click', () => {
+    modalSessionStop();
+    btnResetCounter.classList.add('hidden');
+    paragraph.innerHTML = `You have clicked <b>${counter} times</b> to related button`;
 });
-btnCloseModal.addEventListener(`click`, closeModal);
-overlay.addEventListener(`click`, closeModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//close modal with ESC key for accessibility
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
